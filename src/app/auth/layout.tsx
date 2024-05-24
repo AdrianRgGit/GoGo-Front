29a3cd5f71e4afd67b911cd5ee59a2eb14ed4440
Adrián Ramírez <adrianramirezgalera@gmail.com>
2024-05-24 12:53:01 +0200

    no he podido solucionar el layout de auth

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,8 +1,7 @@
-// app/auth/layout.tsx
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-  return <div className="auth-layout">{children}</div>;
+  return <>{children}</>;
 };
 
 export default AuthLayout;
--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,9 +3,9 @@ import React from "react";
 
 const AuthPage = () => {
   return (
-    <main>
+    <div>
       <h1>Auth</h1>
-    </main>
+    </div>
   );
 };
 
